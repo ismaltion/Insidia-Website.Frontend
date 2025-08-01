3e35c045100915c5f283aa9823c9e123d392472e
Maltion <89048323+ismaltion@users.noreply.github.com>
2025-08-01 22:10:03 +0000

    Started the first html document, stylesheet and JavaScript.

---
+++ scripts/common.js
@@ -0,0 +1,15 @@
+const header = document.getElementById("main-header");
+
+header.innerHTML = `
+<div class="iheader">
+    <a href="index.html"><span class="logo">
+        <h2>INSIDIA</h2>
+    </span></a>
+    <nav>
+        <a href="index.html">Home</a>
+        <a href="install.html">Install</a>
+        <a href="docs.html">Docs</a>
+        <a href="forum.html">Forum</a>
+    </nav>
+</div>
+`;
