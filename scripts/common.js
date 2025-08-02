b58b7e2ab71296ebfb1a7ac0658615a57a57e616
Maltion <89048323+ismaltion@users.noreply.github.com>
2025-08-02 17:59:43 +0000

    added a lot of pages

--- scripts/common.js
+++ scripts/common.js
@@ -1,4 +1,5 @@
 const header = document.getElementById("main-header");
+const footer = document.getElementById("main-footer");
 
 header.innerHTML = `
 <div class="iheader">
@@ -10,6 +11,41 @@ header.innerHTML = `
         <a href="install.html">Install</a>
         <a href="docs.html">Docs</a>
         <a href="forum.html">Forum</a>
+        <span class="meui-button4">Login</span>
     </nav>
 </div>
 `;
+
+// Missing links here btw
+footer.innerHTML = `
+    <div class="ifooter">
+        <nav>
+            <h4>About</h4>
+            <a href="about.html">About Insidia</a>
+            <a href="docs.html">Docs</a>
+            <a href="docs.html?=">Mod creation</a>
+        </nav>
+        <nav>
+            <h4>Social</h4>
+            <a href="forum.html">Insidia Forum</a>
+            <a href="friendlist.html">Friend list</a>
+            <a href="profile.html?=">Profile</a>
+        </nav>
+        <nav>
+            <h4>More from Exdash</h4>
+            <a href="https://anonynmous-exdash.github.io/">Home</a>
+            <a href="https://anonynmous-exdash.github.io/project-loader/">DEX Dash</a>
+            <a href="https://anonynmous-exdash.github.io/cloud-id-switcher/">AEX Dash</a>
+            <a href="https://github.com/anonynmous-exdash/">GitHub Profile</a>
+            <a href="https://scratch.mit.edu/users/anon-exdash/">Scratch Profile</a>
+        </nav>
+        <nav>
+            <h4>More from Maltion</h4>
+            <a href="https://www.maltion.com">Home</a>
+            <a href="https://mclient.maltion.com">MClient</a>
+            <a href="https://voxarc.maltion.com">Voxarc</a>
+            <a href="https://github.com/ismaltion">GitHub Profile</a>
+            <a href="https://scratch.mit.edu/users/Maltion/">Scratch Profile</a>
+        </nav>
+    </div>
+`
